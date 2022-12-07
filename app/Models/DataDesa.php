8d0b44b40d1c1168a8780bc5b9099543ce3621e7
applepie25 <hanaulfia1612@gmail.com>
2022-12-07 23:38:11 +0700

    get berita-desa api

---
+++ app/Models/DataDesa.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class DataDesa extends Model
+{
+    use HasFactory;
+    protected $table = 'das_data_desa';
+    protected $fillable = ['desa_id', 'nama', 'luas_wilayah', 'website', 'path'];
+
+    public function getWebsiteUrlFeedAttribute()
+    {
+        if (Str::endsWith($this->website, '/') == false) {
+            $this->website .= '/';
+        }
+
+        $desa = [
+            'desa_id' => $this->desa_id,
+            'nama'    => ucwords($this->sebutan_desa . ' ' . $this->nama),
+            'website' => $this->website . 'index.php/feed'
+        ];
+        return $desa;
+    }
+
+    public function scopeWebsiteUrl($query)
+    {
+        return $query->whereNotNull('website');
+    }
+}
